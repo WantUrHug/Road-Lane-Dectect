2ec55c1c470948bc65d5995c6fa72b2fb3f30bff
WantUrHug <602141901@qq.com>
2019-08-27 23:37:46 +0800

    test

--- test.py
+++ test.py
@@ -4,15 +4,23 @@ import os
 from utils import VideoGo
 
 test_img = "D:\\0824\\Project1\\Project1\\test_videos\\pic_001.jpg"
-#im = cv2.imread(test_img, cv2.IMREAD_GRAYSCALE)
-im = cv2.imread(test_img)
-im = im[450:, :, :]
+#读取灰度图像信息，因为后续要进行一些二值化之后的边缘检测
+im = cv2.imread(test_img, cv2.IMREAD_GRAYSCALE)
 
-h, w, _ = im.shape
+#读取图片上方一部分，因为考虑到消失点的缘故，消失点上方的空间是
+#无法被呈现在俯瞰图中的
+im = im[450:, :]
+#确定现处理图片的尺寸
+h, w = im.shape
 print(h, w)
 cv2.imshow("windows", im)
+#展示500ms
 cv2.waitKey(500)
 cv2.destroyAllWindows()
+
+
+
+'''
 pts = np.float32([[0,0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]])
 pts1 = np.float32([[0,0], [w/2 - 1 - 100, h - 1], [w/2 - 1 + 100, h - 1], [w - 1, 0]])
 
@@ -23,3 +31,4 @@ dst = cv2.warpPerspective(im,M, dsize = (1280, 270))
 cv2.imshow("windows", dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+'''
